c92ed3b10e0871ea4656fce5da62d91aa2dea789
Juan Pablo Urista <jurista@andrew.cmu.edu>
2024-09-25 22:46:43 -0400

    updated project structure

--- enableElements.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", runall);
-
-function runall () {
-        const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-        const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-}
